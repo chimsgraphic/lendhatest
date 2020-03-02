b5ead9738d8bdabc89a81c651c5cba25b69e477f
Chimezie Mbagwu <chimsgraphic@gmail.com>
2020-03-02 05:30:50 -0800

    Updated Code

--- app/Http/Controllers/CreateProducts.php
+++ app/Http/Controllers/CreateProducts.php
@@ -7,4 +7,19 @@ use Illuminate\Http\Request;
 class CreateProducts extends Controller
 {
     //
+	
+	public function create(Request $request)
+	{
+		
+		        $data = $request->json()->all();
+		       $pname = $data['product_name'];
+		 $description = $data['description'];
+		
+		 DB::table('tbl_products')->insert(['name' => $pname, 'description' => $description]);
+		 
+         return response()->json(['error'=>'00','message'=>'New Product Created!']);
+          //return response()->json(['error'=>'01','message'=>'Notification confirmation failed']);
+                  
+            
+	}
 }
